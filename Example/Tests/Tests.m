5d1fe753d7d31ce69e3d77bcdd94d1fbb77fad38
Alexander Widerberg <alexander.widerberg@cloudme.com>
2014-10-29 09:55:06 +0100

    Removed test stub

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -8,28 +8,3 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-
-@interface Tests : XCTestCase
-
-@end
-
-@implementation Tests
-
-- (void)setUp {
-    [super setUp];
-}
-
-- (void)tearDown {
-    [super tearDown];
-}
-
-- (void)test {
-    XCTAssert(YES, @"Dummy Pass");
-}
-
-- (void)testPerformance {
-    [self measureBlock:^{
-    }];
-}
-
-@end
